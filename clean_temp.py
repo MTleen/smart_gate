07488c1cc0dfb497fefa41fb195d7c1815ee3f56
MTleen <673719729@qq.com>
2020-07-02 16:13:13 +0800

    修改照片保存时间

--- clean_temp.py
+++ clean_temp.py
@@ -4,7 +4,7 @@ import shutil
 if __name__ == '__main__':
     file_list = os.listdir('./temp')
     file_list = sorted(file_list)
-    backup_days = 1
+    backup_days = 15
     if len(file_list) > backup_days:
         for dir_path in file_list[:-backup_days]:
             if os.path.isdir(os.path.join('./temp', dir_path)):
